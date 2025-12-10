e8d315772e1a170b92c86de1592a3f1cc15e3ebf
Franco <fuschendel@gmail.com>
2025-12-09 23:30:14 -0300

    app

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ import { connectDB } from './src/config/db.js';
 import { authentication } from './src/middlewares/authentication.js';
 
 const app = express();
-const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,29 +16,8 @@ await connectDB();
 app.use('/api', productsRouter);
 app.use('/', loginRouter);
 
-/* 
-app.get('/ping', (req, res) => {
-    res.end('/pong');
-});
-
-app.get('/html', (req, res) => {
-    res.end(`<h1>Bienvenidos</h1>`);
-});
-
-app.get('/json', (req, res) => {
-    res.json({"nombre":"Franco", "edad": 20});
-});
-
-app.get('/usuarios/:id', (req, res) => {
-    const id = req.params.id
-    res.end(`Toma el usuario ${id}`);
-});
-*/
-
 app.use((req, res, next) => {
     res.status(404).send("ERROR: Solicitud incorrecta");
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor WEB creado en http://localhost:${PORT}`);
-});
+export default app;
